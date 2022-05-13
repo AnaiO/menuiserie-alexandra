e9417d7e013b2d9b37256998e3ff4427608b2e35
AnaiO <anais.berton.io@gmail.com>
2022-05-13 21:05:38 +0200

    add tailwindcss + layout generalities app.blade

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './storage/framework/views/*.php',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+  ],  
+  theme: {
+    colors: {
+      'blue': '#1fb6ff',
+      // 'light-blue': '',
+      // 'dark-blue': '',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
